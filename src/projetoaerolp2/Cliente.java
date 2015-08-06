050cc458caa11e820d6f23e492ea79768a686f28
Logan Carvalho <logansa@hotmail.com>
2015-08-06 10:16:39 -0300

    Começando do zero com cadastro de clientes e usando LInkedList.

---
+++ src/projetoaerolp2/Cliente.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoaerolp2;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ *
+ * @author Logan
+ */
+public class Cliente {
+    
+    private String nome;
+
+    public static final String PROP_NOME = "nome";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        String oldNome = this.nome;
+        this.nome = nome;
+        propertyChangeSupport.firePropertyChange(PROP_NOME, oldNome, nome);
+    }
+
+    private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
+    private String CPF;
+
+    public static final String PROP_CPF = "CPF";
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        String oldCPF = this.CPF;
+        this.CPF = CPF;
+        propertyChangeSupport.firePropertyChange(PROP_CPF, oldCPF, CPF);
+    }
+
+    
+}
---
+++ src/projetoaerolp2/ProjetoAeroLP2.java
@@ -0,0 +1,19 @@
+
+package projetoaerolp2;
+
+/**
+ *
+ * @author Logan
+ */
+public class ProjetoAeroLP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Criando objeto e tornando-o visivel no main
+        CadastroCliente cadCli = new CadastroCliente();
+        cadCli.setVisible(true);
+    }
+    
+}
