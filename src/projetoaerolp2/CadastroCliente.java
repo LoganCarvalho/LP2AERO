c6556da5b8f750d360d8ffe6843bd5878a0f08cd
Logan Carvalho <logansa@hotmail.com>
2015-08-13 10:16:46 -0300

    Classe de conexão terminada e funcionando com sucesso. (Necessário fazer modificações para outro localhost)

--- src/projetoaerolp2/CadastroCliente.java
+++ src/projetoaerolp2/CadastroCliente.java
@@ -6,21 +6,18 @@
 package projetoaerolp2;
 
 import java.util.LinkedList;
-import java.sql.*;
-import DAL.ConectBd;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import utilitarios.ConectaBanco;
 
 public class CadastroCliente extends javax.swing.JFrame {
-
-    Connection con = null;
-    PreparedStatement pst = null;
-    ResultSet rs = null;
+    ConectaBanco conexao = new ConectaBanco();//variavel global
+    
     
     public CadastroCliente() throws ClassNotFoundException {
         initComponents();
-        this.setLocationRelativeTo(null);
-        con = ConectBd.conectbd();
+        conexao.conectar();
+        
     }
 
     /**
@@ -113,28 +110,7 @@ public class CadastroCliente extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastroCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastroCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastroCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastroCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+        
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
