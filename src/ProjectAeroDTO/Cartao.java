518724f42908baf0b3d656695e35d7dde2c36d98
slrocha <slrocha17@gmail.com>
2015-09-02 10:13:20 -0300

    Construido construtor na classe cartao

--- src/ProjectAeroDTO/Cartao.java
+++ src/ProjectAeroDTO/Cartao.java
@@ -2,10 +2,15 @@ package ProjectAeroDTO;
 
 
 public class Cartao {
-    
+       
     private int Id;
     private int num_Pontos;
 
+    public Cartao(int Id, int num_Pontos) {
+        this.Id = Id;
+        this.num_Pontos = num_Pontos;
+    }
+    
     public int getNum_Pontos() {
         return num_Pontos;
     }
